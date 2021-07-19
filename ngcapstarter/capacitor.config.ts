3b7f10d20ed7fde3b1f08fccc02179cc28fa630a
Sasha <ashatilovdev@gmail.com>
2021-07-19 21:39:58 +0300

    npx cap init npx cap run ios

--- ngcapstarter/capacitor.config.ts
+++ ngcapstarter/capacitor.config.ts
@@ -3,8 +3,13 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'ngcapstarter',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist/ngcapstarter',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+  }
 };
 
 export default config;
